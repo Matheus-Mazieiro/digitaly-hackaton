b559d6c262ead36a0a43a5b705299eb6aaecce35
MatheusMazieiro <matheus_mazieiro@hotmail.com>
2026-09-12 17:52:22 -0300

    feat: some filters

--- back/internal/model/doctor/doctor.go
+++ back/internal/model/doctor/doctor.go
@@ -16,3 +16,18 @@ type Doctor struct {
 	Especialidade string    `json:"especialidade"`
 	Senha         string    `json:"senha"`
 }
+
+type DoctorFilter struct {
+	ID            string `json:"id"`
+	Nome          string `json:"nome"`
+	Email         string `json:"email"`
+	Telefone      string `json:"telefone"`
+	Nascimento    string `json:"nascimento"`
+	CPF           string `json:"cpf"`
+	CRM           string `json:"crm"`
+	Biografia     string `json:"biografia"`
+	AvaliacaoSoma string `json:"avaliacao_soma"`
+	NConsultas    string `json:"n_consultas"`
+	Especialidade string `json:"especialidade"`
+	Senha         string `json:"senha"`
+}
