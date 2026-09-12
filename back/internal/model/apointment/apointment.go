b6c489d75c42c9bae61592a4459c8380c2d4dade
MatheusMazieiro <matheus_mazieiro@hotmail.com>
2026-09-12 18:00:02 -0300

    feat: more filter

--- back/internal/model/apointment/apointment.go
+++ back/internal/model/apointment/apointment.go
@@ -14,3 +14,16 @@ type Apointment struct {
 	Link       string    `json:"link"`
 	Reason     string    `json:"reason"`
 }
+
+type AppointmentFilter struct {
+	ID         string `json:"id"`
+	Paciente   string `json:"paciente"`
+	Medico     string `json:"medico"`
+	Hora       string `json:"hora"`
+	Status     string `json:"status"`
+	Prontuario string `json:"prontuario"`
+	Resumo     string `json:"resumo"`
+	Receita    string `json:"receita"`
+	Link       string `json:"link"`
+	Reason     string `json:"reason"`
+}
